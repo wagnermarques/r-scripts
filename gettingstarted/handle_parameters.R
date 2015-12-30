a65f70920a7aabbf87313d5d0bc5e3796a7f2f81
wagnermarques <wagnerdocri@gmail.com>
2015-12-30 03:05:31 -0200

    just started R script to learn... for now just a download functionality is impemented

---
+++ gettingstarted/handle_parameters.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+#handle parameters
+
+
+
+handleParameters <- function(args){
+    for (i in 1:length(args)) {
+        print(paste("arg",as.character(i),"=",args[i]))
+    }
+    return(object)
+}
+
+
+
